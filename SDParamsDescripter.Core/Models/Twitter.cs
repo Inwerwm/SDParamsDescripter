89491e00dba534eb0c798c1f9f2e9aaeeed8d82f
Inwerwm <saclainvervm@gmail.com>
2022-09-04 02:04:39 +0900

    画像付きツイートを追加

--- SDParamsDescripter.Core/Models/Twitter.cs
+++ SDParamsDescripter.Core/Models/Twitter.cs
@@ -1,17 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using LinqToTwitter;
+using LinqToTwitter.OAuth;
 
 namespace SDParamsDescripter.Core.Models;
 public class Twitter
 {
-    public Twitter(string key, string secret)
+    private TwitterContext Context
     {
+        get;
     }
 
-    public async Task Post(string text, string imagePath, string alt)
+    public Twitter(string consumerKey, string consumerSecret, string accessToken, string accessTokenSecret)
     {
+        var auth = new SingleUserAuthorizer
+        {
+            CredentialStore = new SingleUserInMemoryCredentialStore
+            {
+                ConsumerKey = consumerKey,
+                ConsumerSecret = consumerSecret,
+                AccessToken = accessToken,
+                AccessTokenSecret = accessTokenSecret
+            }
+        };
+
+        Context = new TwitterContext(auth);
+    }
+
+    public async Task TweetWithMedia(string text, string imagePath, string imageAltText)
+    {
+        var image = File.ReadAllBytes(imagePath);
+        var media = await Context.UploadMediaAsync(image, "image/png", "tweet_image");
+        await Context.CreateMediaMetadataAsync(media.MediaID, imageAltText);
+        await Context.TweetMediaAsync(text, new[] { media.MediaID.ToString() });
     }
 }
