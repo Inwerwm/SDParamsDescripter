5e256d747e0d9c197f633e0c004af574e33bd70d
Inwerwm <saclainvervm@gmail.com>
2022-09-10 10:35:01 +0900

    不要なusingの削除

--- SDParamsDescripter.Core/Twitter/Tweet.cs
+++ SDParamsDescripter.Core/Twitter/Tweet.cs
@@ -1,8 +1,2 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SDParamsDescripter.Core.Twitter;
+﻿namespace SDParamsDescripter.Core.Twitter;
 public record Tweet(string Text, string ImagePath, string ImageAltText, bool ResizeImageWhenTooLarge);
